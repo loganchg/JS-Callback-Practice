9358b531fd19ed6f8006083502618cd21a8f9be1
Logan Cheng <loganchg@hotmail.com>
2022-02-04 01:08:20 -0800

    new changes

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+let direction = null;
+let x = 100;
+let y = 250;
 
+if(direction === 'west'){
+    x = x - 1
+}
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
