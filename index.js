7bfc71418db3a177a763afc9d8453738dcd764f1
Logan Cheng <loganchg@hotmail.com>
2022-02-12 18:14:42 -0800

    new chnages

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let direction = null;
 let x = 100;
 let y = 250;
 
-setInterval(function(){ 
+function moveCharacter(){ 
     if(direction === 'west'){
         x = x - 1
     }
@@ -22,30 +22,7 @@ setInterval(function(){
     }
     character.style.left = x + 'px'
     character.style.bottom = y + 'px'
-}, 1)
-
-
-
-document.addEventListener('keydown', function(e){
-    if(e.repeat) return;
-
-    if(e.key === 'ArrowLeft'){
-        direction = 'west'
-    }
-    if(e.key === 'ArrowUp'){
-        direction = 'north'
-    }
-    if(e.key === 'ArrowRight'){
-        direction = 'east'
-    }
-    if(e.key === 'ArrowDown'){
-        direction = 'south'
-    }
-})
-
-document.addEventListener('keyup', function(e){
-    direction = null
-})
+}
 
 
 
--- move.js
+++ move.js
@@ -6,9 +6,60 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
     
-
+//arrows
     function moveWithArrowKeys(left, bottom){
-       
+        function moveWithArrowKeys(left, bottom, callback){
+            let direction = null;
+            let x = 100;
+            let y = 250;
+           
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px';
+    
+//functions
+            function moveCharacter(){
+                if(directions === 'west'){
+                    x = x - 1
+                }
+                
+                if(direction === 'north'){
+                    y = y + 1
+                }
+                if(direction === 'east'){
+                    x = x + 1
+                }
+                if(direction === 'south'){
+                    y = y - 1
+                }
+                element.style.left = x + 'px'
+                element.style.bottom = y + 'px';
+            }
+
+            setInterval(moveCharacter, 1)
+
+            document.addEventListener('keydown', function(e){
+                if(e.repeat) return;
+            
+                if(e.key === 'ArrowLeft'){
+                    direction = 'west'
+                }
+                if(e.key === 'ArrowUp'){
+                    direction = 'north'
+                }
+                if(e.key === 'ArrowRight'){
+                    direction = 'east'
+                }
+                if(e.key === 'ArrowDown'){
+                    direction = 'south'
+                }
+                callback(direction)
+            })
+            
+            document.addEventListener('keyup', function(e){
+                direction = null
+                callback(direction)
+            })
+        }
     }
 
     return {
